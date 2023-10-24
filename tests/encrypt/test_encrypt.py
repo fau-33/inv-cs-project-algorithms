ec3842050d1002b9331c2b74841e1ff956597314
Flávio Félix <fau_felix30@hotmail.com>
2023-10-24 19:22:14 -0300

    challenge encrypt message tests

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,20 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    message = 'myMessage'
+    reversed_message = 'egasseMym'
+    message_with_even_key = 'egass_eMym'
+    message_with_odd_key = 'Mym_egasse'
+
+    assert encrypt_message(message, 99) == reversed_message
+    assert encrypt_message(message, 4) == message_with_even_key
+    assert encrypt_message(message, 3) == message_with_odd_key
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(message, '3')
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(9999, 3)
