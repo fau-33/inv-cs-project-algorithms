a7ee5b8531ac48f6a9a54b3d5a1e927bd6c4dc49
Flávio Félix <fau_felix30@hotmail.com>
2023-10-24 19:03:42 -0300

    Challenge study schedulece done

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None or permanence_period is None:
+        return None
+    students = 0
+
+    for period in permanence_period:
+        if not isinstance(period[0], int) or not isinstance(period[1], int):
+            return None
+        if period[0] <= target_time <= period[1]:
+            students += 1
+    return students
