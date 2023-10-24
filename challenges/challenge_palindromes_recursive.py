46db39ac5943fe748267b5440ade396d7a3e71d6
Flávio Félix <fau_felix30@hotmail.com>
2023-10-24 19:33:09 -0300

    challenge palindrome recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+    if low_index >= high_index:
+        return True
+    normal_word = word
+    reverse_word = word[::-1]
+
+    if normal_word == reverse_word:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return False
